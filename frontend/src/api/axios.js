c72ca4de1098452ea03f8d0e945ccc1b51f590af
DEVENDRA-5470 <your_email@example.com>
2025-12-05 19:52:52 +0530

    New-build ✅

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://52.15.78.46:32000/api", // Backend URL
+  baseURL: "http://3.12.76.57:32001/api", // Backend URL
   withCredentials: true,                // allow cookies/token if used later
 });
 
